9e6f62e15a4f5469b4e99f509c61b05935f50f84
Ali Sher Khan <72862093+Aaslan01@users.noreply.github.com>
2023-03-02 19:13:01 +0500

    🛠️

---
+++ data/colors.js
@@ -0,0 +1,12 @@
+const StyleGuide = {
+  colors: {
+    black: '#000',
+    dark: `rgb(70,70,70)`,
+    yellow: `rgb(250,230,50)`,
+    gray: `rgb(120,120,120)`,
+    light: `rgb(240,240,240)`,
+    white: '#fff',
+  },
+};
+
+export default StyleGuide;
--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -1,10 +1,66 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import {View, Text, ImageBackground, TouchableOpacity} from 'react-native';
+import React from 'react';
+import StyleGuide from '../data/colors';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 export default function MainScreen() {
   return (
-    <View>
-      <Text>MainScreen</Text>
-    </View>
-  )
+    <ImageBackground
+      style={{flex: 1}}
+      source={require('../assets/background-image.jpeg')}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: StyleGuide.colors.black,
+          opacity: 0.4,
+        }}
+      />
+      <View
+        style={{
+          height: hp(96),
+          width: wp(100),
+          position: 'absolute',
+          zIndex: 2,
+          padding: wp(4),
+          justifyContent: 'flex-end',
+        }}>
+        <Text
+          style={{
+            color: StyleGuide.colors.white,
+            fontSize: hp(4.8),
+            fontFamily: 'Poppins-Bold',
+          }}>
+          Let Your Favorite Food Find You
+        </Text>
+        <Text
+          style={{
+            color: StyleGuide.colors.light,
+            fontSize: hp(2),
+            fontFamily: 'Poppins-Regular',
+          }}>
+          It is a long established fact that a reader will be distracted by the
+          readable page when looking at
+        </Text>
+        <TouchableOpacity
+          style={{
+            backgroundColor: StyleGuide.colors.white,
+            width: wp(92),
+            paddingVertical: hp(2.2),
+            borderRadius: 20,
+            alignItems: 'center',
+          }}>
+          <Text
+            style={{
+              color: StyleGuide.colors.black,
+              fontFamily: 'Poppins-Bold',
+              fontSize: hp(2),
+            }}>
+            Explore Now
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
 }
