e10f7d4cbe6f149c33a76e7df784491da3355264
Ali Sher Khan <72862093+Aaslan01@users.noreply.github.com>
2023-03-02 18:24:23 +0500

    :robot_face:

---
+++ App.tsx
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import MainScreen from './screens/MainScreen';
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      {
+        <Stack.Navigator initialRouteName="Opening">
+          <Stack.Screen
+            name="Opening"
+            component={MainScreen}
+            options={{header: () => null}}
+          />
+          {/* <Stack.Screen
+            name="LoginScreen"
+            component={LoginScreen}
+            options={{header: () => null}}
+          /> */}
+        </Stack.Navigator>
+      }
+    </NavigationContainer>
+  );
+}
+
+
+export default App;
